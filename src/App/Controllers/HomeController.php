9e7eef0f129bea66b76d806a04889725618702f5
Carollous Dachi <166882001+carollousdev@users.noreply.github.com>
2024-11-01 21:02:43 +0700

    Create template engine

--- src/App/Controllers/HomeController.php
+++ src/App/Controllers/HomeController.php
@@ -4,10 +4,21 @@ declare(strict_types=1);
 
 namespace App\Controllers;
 
+use Framework\TemplateEngine;
+
 class HomeController
 {
+
+    private TemplateEngine $view;
+
+    public function __construct()
+    {
+        $this->view = new TemplateEngine();
+    }
+
     public function Home()
     {
+        dd($this->view);
         echo 'Home Page';
     }
 }
