bcb7e3ad52222fae3bf73092f91456fc341d682e
Carollous Dachi <166882001+carollousdev@users.noreply.github.com>
2024-11-04 16:26:05 +0700

    Creating container

--- src/App/Framework/App.php
+++ src/App/Framework/App.php
@@ -6,13 +6,18 @@ declare(strict_types=1);
 
 namespace Framework;
 
+use Framework\Container;
+
 class App
 {
+
     private Router $router;
+    private Container $container;
 
     public function __construct()
     {
         $this->router = new Router();
+        $this->container = new Container();
     }
 
     public function run()
