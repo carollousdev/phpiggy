f5dbe515b3cbbc20bbbad3c5ebd568cdddf0b9b3
Carollous Dachi <166882001+carollousdev@users.noreply.github.com>
2024-11-07 18:55:10 +0700

    Understanding service

--- src/App/container-definitions.php
+++ src/App/container-definitions.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 use Framework\TemplateEngine;
 use App\Config\paths;
+use App\Services\ValidatorService;
 
 return [
     TemplateEngine::class => fn() => new TemplateEngine(paths::VIEW),
+    ValidatorService::class => fn() => new ValidatorService(),
 ];
